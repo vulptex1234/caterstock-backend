2e97a36d46104921ee1cdf3085b83b9a3ad69dc6
vulptex <g21240091a@edu.teu.ac.jp>
2025-05-30 09:20:20 +0900

    change CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 # CORS設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://caterstock-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
